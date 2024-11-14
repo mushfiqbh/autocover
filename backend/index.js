addc04e33d65116c623c553720c95f7f7701e066
mushfiqbh <mushfiqbh@gmail.com>
2024-11-14 16:59:41 +0600

    Use Puppyteer

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,36 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const pdf = require("html-pdf");
+const puppeteer = require("puppeteer");
 const cors = require("cors");
 
 const pdfTemplate = require("./documents");
-
 const app = express();
-
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.post("/create-pdf", (req, res) => {
-  pdf.create(pdfTemplate(req.body), {}).toFile("result.pdf", (err) => {
-    if (err) {
-      res.send(Promise.reject());
-    }
+app.post("/create-pdf", async (req, res) => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    const content = pdfTemplate(req.body);
 
-    res.send(Promise.resolve());
-  });
+    await page.setContent(content);
+    await page.emulateMediaType('screen');
+    const pdfBuffer = await page.pdf({ format: 'A4' });
+
+    await browser.close();
+
+    res.set({
+      'Content-Type': 'application/pdf',
+      'Content-Length': pdfBuffer.length
+    });
+    res.send(pdfBuffer);
+  } catch (err) {
+    res.status(500).send(Promise.reject());
+  }
 });
 
 app.get("/fetch-pdf", (req, res) => {
